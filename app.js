f5f4124316aea9830bc67f759cb1efe1ee87e643
Cunxiang Yu <cunxiangyu@gmail.com>
2017-09-30 15:44:37 +1000

    Deploy on heroku

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Start app
 const app = express();
 
-// Use Handlebars Middleware and setup view engine
-app.engine('handlebars', exphbs({
-  defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+// // Connect to mLab database
+// mongoose.Promise = global.Promise // Map global promise
+// mongoose.connect('', {
+//   useMongoClient: true
+// })
+//   .then(() => console.log('MongoDB successfully connected...'))
+//   .catch(err => console.log(err));
+//
+// // Use Handlebars Middleware and setup view engine
+// app.engine('handlebars', exphbs({
+//   defaultLayout: 'main'
+// }));
+// app.set('view engine', 'handlebars');
+
+// // Use Body-parser Middleware
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 // Index Route
 app.get('/', (req, res) => {
-
+  res.send('It works!');
 });
 
-const port = 5000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
