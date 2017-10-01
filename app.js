8a9578505420d859002d017afc5ab69e589b4812
Cunxiang Yu <cunxiangyu@gmail.com>
2017-10-01 14:22:24 +1100

    Installed mongoose, conencted app to mLab database and built the index page and route

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ const exphbs = require('express-handlebars');
 // Init app
 const app = express();
 
+// Connect to database
+mongoose.Promise = global.Promise; // Map global promise
+const config = require('./config/credentials');
+
+mongoose.connect(config.db, {
+  useMongoClient: true
+})
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.log(err));
+
 // Handlebars Middleware
 app.engine('handlebars', exphbs({
   defaultLayout: 'main'
@@ -18,7 +28,7 @@ app.get('/', (req, res) => {
 
 
 // Set port for production and development
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
