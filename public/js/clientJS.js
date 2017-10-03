97eee330402e4f3ed07f23ca334ff04900bf50ed
Cunxiang Yu <cunxiangyu@gmail.com>
2017-10-03 17:13:25 +1100

    Integrated with FreeCodeCamp weather API

--- public/js/clientJS.js
+++ public/js/clientJS.js
@@ -1,3 +1,52 @@
+// Function for making Ajax requst to target FCC url
+// and get the local weather
+function getWeather(api, lat, lon, temp) {
+  var urlString = api + lat + "&" + lon;
+  $.ajax({
+    url: urlString,
+    success: function (result) {
+      $("#city").text(result.name + ", ");
+      $("#country").text(result.sys.country);
+      temp = Math.round(result.main.temp * 10) / 10;
+      $("#temp").text(temp + " " + String.fromCharCode(176) + 'C');
+      $("#desc").text(result.weather[0].main);
+      IconGen(result.weather[0].main);
+  }
+  });
+}
+
+// Function for getting the target weather icon
+function IconGen(desc) {
+  var desc = desc.toLowerCase()
+  switch (desc) {
+    case 'drizzle':
+      addIcon(desc)
+      break;
+    case 'clouds':
+      addIcon(desc)
+      break;
+    case 'rain':
+      addIcon(desc)
+      break;
+    case 'snow':
+      addIcon(desc)
+      break;
+    case 'clear':
+      addIcon(desc)
+      break;
+    case 'thunderstom':
+      addIcon(desc)
+      break;
+    default:
+      $('div.clouds').removeClass('hide');
+  }
+}
+
+// Function for showing the weather icon
+function addIcon(desc) {
+  $('div.' + desc).removeClass('hide');
+}
+
 $(document).ready(() => {
   // If user is seeing the index page of blogs
   // where an announcement alert is shown
@@ -9,6 +58,29 @@ $(document).ready(() => {
         $('#announcementDiv').show(1500);
       }, 2000)
     });
+  }
+
+  // If user is seeing the index page of blogs
+  // where his/her local weather is shown
+  if ($('#weatherCard').length) {
+    // Weather API provided by FreeCodeCamp
+    var api = "https://fcc-weather-api.glitch.me/api/current?";
+    var lat,
+        lon,
+        temp;
+    // Check if client browser enables getting geolocation
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var lat = "lat=" + position.coords.latitude;
+        var lon = "lon=" + position.coords.longitude;
+        getWeather(api, lat, lon);
+        $('#weatherCard').show(2000);
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+
+
   }
 
   // If there is a textarea for writing blog content
